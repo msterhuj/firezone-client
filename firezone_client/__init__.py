d6fc3d7d79f6d7f942d507f1e9a431a3d0639ede
msterhuj <gabin.lanore@gmail.com>
2023-08-07 20:36:18 +0200

    feat: read and edit config

--- firezone_client/__init__.py
+++ firezone_client/__init__.py
@@ -21,6 +21,53 @@ class User:
         return user
 
 
+class Configurations:
+    allow_unprivileged_device_configuration: bool
+    allow_unprivileged_device_management: bool
+    default_client_allowed_ips: list
+    default_client_dns: list
+    default_client_endpoint: str
+    default_client_mtu: int
+    default_client_persistent_keepalive: int
+    disable_vpn_on_oidc_error: bool
+    id: str
+    inserted_at: datetime
+    local_auth_enabled: bool
+    logo: dict
+    openid_connect_providers: list
+    saml_identity_providers: list
+    updated_at: datetime
+    vpn_session_duration: int
+
+    @staticmethod
+    def __init_from_dict__(data: dict) -> 'Configurations':
+        configurations = Configurations()
+        configurations.__dict__.update(data)
+        return configurations
+    
+    @staticmethod
+    def get(client) -> 'Configurations':
+        return Configurations.__init_from_dict__(client.__get__("/configuration")["data"])
+
+    def patch(self, client):
+        data = {
+            "configuration": {
+                "allow_unprivileged_device_configuration": self.allow_unprivileged_device_configuration,
+                "allow_unprivileged_device_management": self.allow_unprivileged_device_management,
+                "default_client_allowed_ips": self.default_client_allowed_ips,
+                "default_client_dns": self.default_client_dns,
+                "default_client_endpoint": self.default_client_endpoint,
+                "default_client_mtu": self.default_client_mtu,
+                "default_client_persistent_keepalive": self.default_client_persistent_keepalive,
+                "disable_vpn_on_oidc_error": self.disable_vpn_on_oidc_error,
+                "local_auth_enabled": self.local_auth_enabled,
+                "openid_connect_providers": self.openid_connect_providers,
+                "saml_identity_providers": self.saml_identity_providers,
+                "vpn_session_duration": self.vpn_session_duration
+            }
+        }
+        client.__patch__("/configuration", data)
+
 class FZClient:
     endpoint: str
     ssl_verify: bool = True
@@ -37,11 +84,23 @@ class FZClient:
             "Authorization": f"Bearer {self.token}"
         })
 
-    def __get__(self, url: str)-> json:
+    def __get__(self, url: str) -> json:
         reply = requests.get(url=self.endpoint + url, headers=self.headers, verify=self.ssl_verify)
         if reply.status_code == 500:
             raise Exception("Error for request API")
         return reply.json()
+    
+    def __patch__(self, url: str, body) -> json:
+        reply = requests.patch(url=self.endpoint + url, headers=self.headers, verify=self.ssl_verify, json=body)
+        if reply.status_code == 500:
+            raise Exception("Error for request API")
+        return reply.json()
+    
+    def list(self, obj: object) -> object:
+        return obj.list(self)
+    
+    def get(self, obj: object) -> object:
+        return obj.get(self)
 
     def get_users(self) -> List[User]:
         return [

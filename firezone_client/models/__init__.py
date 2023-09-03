6f2ef1d92bb74a6abefdcb3880a8a393678b68aa
msterhuj <gabin.lanore@gmail.com>
2023-09-03 21:52:40 +0200

    feat: add Rule object for api

--- firezone_client/models/__init__.py
+++ firezone_client/models/__init__.py
@@ -1,3 +1,4 @@
 from firezone_client.models.Configurations import Configurations
 from firezone_client.models.User import User
 from firezone_client.models.Devices import Devices
+from firezone_client.models.Rule import Rule
